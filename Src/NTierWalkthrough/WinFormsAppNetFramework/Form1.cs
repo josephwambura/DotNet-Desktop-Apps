8a2b690c4f1db2cc865b234ec41a1f55eef331fb
Joseph Wambura <joseph.wambura@pycs.co.ke>
2021-06-15 12:27:05 +0300

    Windows Forms Consume WCF

---
+++ Src/NTierWalkthrough/WinFormsAppNetFramework/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsAppNetFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client DataSvc = new ServiceReference1.Service1Client();
+            northwindDataSet.Customers.Merge(DataSvc.GetCustomers());
+            northwindDataSet.Orders.Merge(DataSvc.GetOrders());
+        }
+    }
+}
